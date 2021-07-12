e95e322550970d728e5c9fa963fc177e97df0972
AraujoAlexS <araujo.alexsantos@gmail.com>
2021-07-12 20:38:28 -0300

    Primeira Aula da Alura

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons.js' 
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    background: #d9e6f6;
+    font-family: sans-serif;
+  }
+
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column; 
+  }
+
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
@@ -17,7 +38,7 @@ const theme = {
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+    <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
